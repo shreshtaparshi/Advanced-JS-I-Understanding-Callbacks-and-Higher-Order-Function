c1bfd3778591782fc634d3b368a229e6c540f816
Shreshta Parshi <shreshtaparshi@gmail.com>
2025-11-26 13:05:07 +0530

    Add two new files

---
+++ assignment2111(1).js
@@ -0,0 +1,10 @@
+function displayMessage(name) {
+  console.log(`Hello, ${name}!`);
+}
+function getUserInput(callback) {
+  setTimeout(() => {
+    const username = "Alice";
+    callback(username);
+  }, 1000);
+}
+getUserInput(displayMessage);
